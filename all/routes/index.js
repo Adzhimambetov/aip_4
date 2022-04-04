251b77cf44d76e66c817c3db8f746267a1cfb4dd
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2022-04-04 23:22:32 +0300

    4_2

--- all/routes/index.js
+++ all/routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+/* Страница  */
+router.get('/cristiano', function (req, res, next) {
+  res.send("<h1>Страница Криштиану Роналду</h1>")
+});
+/* Страница */
+router.get('/messi', function (req, res, next) {
+  res.send("<h1>Страница Лионеля Месси</h1>")
+});
+/* Страница  */
+router.get('/benzema', function (req, res, next) {
+  res.send("<h1>Страница Карима Бензема</h1>")
+});
+/* Страница  */
+router.get('/neymar', function (req, res, next) {
+  res.send("<h1>Страница Неймара</h1>")
+});
+/* Страница */
+router.get('/robert', function (req, res, next) {
+  res.send("<h1>Страница Роберта Левандовского</h1>")
 });
 
 module.exports = router;
