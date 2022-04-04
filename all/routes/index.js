6c826998f86087e50184227974cb8ae4b2cbf133
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2022-04-04 23:50:55 +0300

    4_3

--- all/routes/index.js
+++ all/routes/index.js
@@ -7,23 +7,43 @@ router.get('/', function (req, res, next) {
 });
 /* Страница  */
 router.get('/cristiano', function (req, res, next) {
-  res.send("<h1>Страница Криштиану Роналду</h1>")
+  res.render('player', {
+    title: "Криштиану Роналду",
+    picture: "images/Cristiano.png",
+    desc: "Плюшевый медвежонок, игрушка"
+  });
 });
 /* Страница */
 router.get('/messi', function (req, res, next) {
-  res.send("<h1>Страница Лионеля Месси</h1>")
+  res.render('player', {
+    title: "Лионель Месси",
+    picture: "images/messi.png",
+    desc: "Плюшевый медвежонок, игрушка"
+  });
 });
 /* Страница  */
 router.get('/benzema', function (req, res, next) {
-  res.send("<h1>Страница Карима Бензема</h1>")
+  res.render('player', {
+    title: "Карим Бензема",
+    picture: "images/benzema.png",
+    desc: "Плюшевый медвежонок, игрушка"
+  });
 });
 /* Страница  */
 router.get('/neymar', function (req, res, next) {
-  res.send("<h1>Страница Неймара</h1>")
+  res.render('player', {
+    title: "Неймар",
+    picture: "images/neymar.png",
+    desc: "Плюшевый медвежонок, игрушка"
+  });
 });
 /* Страница */
 router.get('/robert', function (req, res, next) {
-  res.send("<h1>Страница Роберта Левандовского</h1>")
+  res.render('player', {
+    title: "Роберт Левандовский",
+    picture: "images/robert.png",
+    desc: "Плюшевый медвежонок, игрушка"
+  });
 });
 
 module.exports = router;
