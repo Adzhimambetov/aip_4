3359f954ff9ab1f2a48713212e49eb6fc7175dad
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2022-06-25 16:01:06 +0300

    10_3

--- all/routes/index.js
+++ all/routes/index.js
@@ -1,49 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Hero = require("../models/hero").Hero
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-/* Страница  */
-router.get('/cristiano', function (req, res, next) {
-  res.render('player', {
-    title: "Криштиану Роналду",
-    picture: "images/Cristiano.png",
-    desc: "Плюшевый медвежонок, игрушка"
-  });
-});
-/* Страница */
-router.get('/messi', function (req, res, next) {
-  res.render('player', {
-    title: "Лионель Месси",
-    picture: "images/messi.png",
-    desc: "Плюшевый медвежонок, игрушка"
-  });
-});
-/* Страница  */
-router.get('/benzema', function (req, res, next) {
-  res.render('player', {
-    title: "Карим Бензема",
-    picture: "images/benzema.png",
-    desc: "Плюшевый медвежонок, игрушка"
-  });
-});
-/* Страница  */
-router.get('/neymar', function (req, res, next) {
-  res.render('player', {
-    title: "Неймар",
-    picture: "images/neymar.png",
-    desc: "Плюшевый медвежонок, игрушка"
-  });
-});
-/* Страница */
-router.get('/robert', function (req, res, next) {
-  res.render('player', {
-    title: "Роберт Левандовский",
-    picture: "images/robert.png",
-    desc: "Плюшевый медвежонок, игрушка"
-  });
+  Hero.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
+    res.render('index', {
+      title: 'Express',
+      menu: menu
+    });
+  })
+
 });
 
 module.exports = router;
