ff1c3620de9973c5e2e117c9b31a606d74447a7d
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2022-06-25 10:57:23 +0300

    6_2-6_3

--- all/createDB.js
+++ all/createDB.js
@@ -1,9 +1,11 @@
 var MongoClient = require("mongodb").MongoClient
+var data = require("./data.js").data
+
 MongoClient.connect("mongodb://localhost:27017/all",
     function (err, db) {
         if (err) throw err
         var collection = db.collection('hero')
-        collection.insertOne({ name: "Рик" }, function (err, result) {
+        collection.insertMany(data, function (err, result) {
             db.close()
         })
     })
