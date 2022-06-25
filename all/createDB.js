e19b8dcd8669b1d77bc25b93101f9eb4ce8d641c
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2022-06-25 12:05:02 +0300

    8

--- all/createDB.js
+++ all/createDB.js
@@ -1,11 +1,15 @@
-var MongoClient = require("mongodb").MongoClient
-var data = require("./data.js").data
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
 
-MongoClient.connect("mongodb://localhost:27017/all",
-    function (err, db) {
-        if (err) throw err
-        var collection = db.collection('hero')
-        collection.insertMany(data, function (err, result) {
-            db.close()
-        })
-    })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.meow = function () {
+    console.log(this.get("name") + " сказал мяу")
+}
+
+var Cat = mongoose.model('Cat', schema)
+
+var kitty = new Cat({ name: 'Пушок' })
+kitty.save(function (err) {
+    kitty.meow()
+})
